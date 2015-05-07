63d84ee4515570bbbf55544c9755924f5cb3d689
ZHAO ZHANG <jocelynz@uchicago.edu>
2015-05-07 16:35:37 -0500

    indentation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
- before_action :find_post, :only => [:show, :edit, :update, :destroy]
+ before_action :find_post, :only => [:show, :edit, :update, :delete]
 
  def find_post
  	@post = Post.find_by(id: params[:id])
